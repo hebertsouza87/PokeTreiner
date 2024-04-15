ad87c834f32f609d025cf14a3756f3ae18671258
Hebert Souza <hebertsouza87@gmail.com>
2024-04-15 17:42:07 -0300

    Initialize Spring Boot project with Gradle

---
+++ src/main/java/com/github/hebertsouza87/pokeTreiner/PokeTreinerApplication.java
@@ -0,0 +1,13 @@
+package com.github.hebertsouza87.pokeTreiner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokeTreinerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PokeTreinerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/hebertsouza87/pokeTreiner/PokeTreinerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.hebertsouza87.pokeTreiner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeTreinerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
